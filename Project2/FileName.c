1b1a7392f02ad9f70a6ab56565a033f7f6e7742a
chaeeun <angelasce@naver.com>
2023-07-04 01:48:44 +0900

    포인터와 주소

--- Project2/FileName.c
+++ Project2/FileName.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int main() {
-		int* p;
-		int num[6] = { 1,2,3,4 };
-		p = num;
-		printf("%d ", p);
-		p++;
-		printf("%d ", p);
-		p++;
-		printf("%d ", p);
+	//포 인 터
+		int *pa; //int형 포인터 변수 pa 선언
+		int n = 3;
+		pa = &n; //pa에 n의 주소 대입
+		printf("%d\n", n); //n의 값
+		printf("%d\n", *pa); //n의 값 (주소(pa)가 가리키는(*) 값을 출력해~)
+		printf("%d\n", pa); //n의 주소
+		printf("%d\n", &n); //n의 주소
+
 }
