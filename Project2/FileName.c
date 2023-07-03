670579c5927efde0b25f0077ac9d9722049c41b4
chaeeun <angelasce@naver.com>
2023-07-04 02:22:42 +0900

    배열, 단일 for문으로 입력받고 다중 for문으로 최솟값 찾기

--- Project2/FileName.c
+++ Project2/FileName.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
-//단일 for문으로 값 할당 
+//단일 for문으로 nxn 배열 값 할당 및 출력
 int main() {
     int num[3][3];
+    int i, j;
     for (int i = 0; i < 9; i++) {
-        num[i / 3][i % 3] = i;
+        scanf("%d", &num[i / 3][i % 3]); //0,0 0,1 0,2 1,0 이런 식으로 입력됨..오
     }
-    for (int i = 0; i < 9; i++) {
-        printf("%d", num[i / 3][i % 3]);
+    int min = num[0][0];
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (num[i][j] < min) {
+                min = num[i][j];
+            }
+        }
     }
+
+        printf("%d", min);
+
     return 0;
 }
