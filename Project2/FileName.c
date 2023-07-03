bc1f86f90c99963a01fa0b304af76fdadb8fa857
chaeeun <angelasce@naver.com>
2023-07-04 00:41:34 +0900

    오른쪽 정렬 직삼각형 모양으로 n줄짜리 별 찍기

--- Project2/FileName.c
+++ Project2/FileName.c
@@ -1,13 +1,28 @@
 #include <stdio.h> 
 
-//06번: A+B를 T줄 만큼 입력 및 출력
+//10번: 오른쪽 정렬 직삼각형 모양으로 n줄짜리 별 찍기
 int main() {
-	int T, i, A, B;
-	scanf("%d", &T);
-	for (i=0;i<T;i++) {
-		scanf("%d %d", &A , &B);
-		printf("%d\n", A + B);
+	int n, i, j, k;
+	scanf("%d", &n);
+	//n개의 줄 출력
+	for (i=1;i<=n;i++) {
+		//공백 출력 (공백은 n-1개씩)
+		for (j=i;j<n;j++) {
+			printf(" ");
+		}
+		//별 출력 (별은 i개수랑 똑같이)
+		for (k=1;k<=i;k++) {
+			printf("*");
+		}
+		//줄바꿈 & 마지막 출력에는 줄바꿈 안되게..
+		if (i != n) {
+			printf("\n");
+		}
+		else {
+			break;
+		}
 	}
 	return 0;
 }
+
 
