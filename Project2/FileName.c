dd66570b55dffddae97d02f10a94dfdf9d1150ee
chaeeun <angelasce@naver.com>
2023-07-04 02:02:12 +0900

    단일 for문으로 nxn 배열 값 할당 및 출력

--- Project2/FileName.c
+++ Project2/FileName.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 
+//단일 for문으로 값 할당 
 int main() {
-	//3x3 배열을 만들어서 아무런 값을 할당
-	int i, k;
-	int n[3][3];
-	for (i=0;i<3;i++) {
-		for (k=0;k<3;k++) {
-			n[i][k] = i * k;
-		}
-	}
-	for (i = 0; i < 3; i++) {
-		for (k = 0; k < 3; k++) {
-			printf("%d", n[i][k]);
-		}
-		printf("\n");
-	}
-		     
+    int num[3][3];
+    for (int i = 0; i < 9; i++) {
+        num[i / 3][i % 3] = i;
+    }
+    for (int i = 0; i < 9; i++) {
+        printf("%d", num[i / 3][i % 3]);
+    }
+    return 0;
 }
