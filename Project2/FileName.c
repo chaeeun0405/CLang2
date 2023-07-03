741381f05a96ae6bb235ddd49ce7b4c7ac9fe84d
chaeeun <angelasce@naver.com>
2023-07-04 00:54:12 +0900

    배열, 쓰레기값, NULL, 배열의 입력 및 출력

--- Project2/FileName.c
+++ Project2/FileName.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 
-//별찍기 다중 for문
+
+//문자형, 문자열, 정수형 배열의 차이점
 int main() {
-    int n;
-    scanf("%d", &n);
-    for (int row = 0; row < n * 2 - 1; row++) {
-        for (int col = 0; col < n * 2 - 1; col++) {
-            if (row < n)
-                if ((n - col - 1 <= row && col - n + 1 <= row))
-                    printf("*");
-                else
-                    printf(" ");
-            else {
-                if ((n - col - 1 <= 2 * (n - 1) - row && col - n + 1 <= 2 * (n - 1) - row))
-                    printf("*");
-                else
-                    printf(" ");
-            }
-        }
-        printf("\n");
+   /* int i;
+    int n[50] = { 0, 1, 2 }; //뒤에 쓰레기값 나옴,, 정수형 배열은 NULL 없음!
+    for (i=0;i<50;i++) {
+        printf("%d", n[i]);
+    }*/
+
+    char n[6] = {'a' , 'b' , 'c' , NULL}; //3개 찍고 멈춰
+    int i;
+    //for (i = 0; i < 6; i++) {
+    //    scanf("%d", &n[i]);
+    //}
+    for (i = 0; i < 6; i++) {
+        printf("%c", n[i]);
     }
-    return 0;
+    printf("%s", n); //%s = str,, 배열의 맨 앞에서부터 NULL까지 출력 
 }
