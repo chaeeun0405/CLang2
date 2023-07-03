bdb845a6a89ed1cb45db7f5e837d192314ba0195
chaeeun <angelasce@naver.com>
2023-07-04 00:42:05 +0900

    T줄만큼 A+B를 출력, 마지막엔 항상 A==0 B==0

--- Project2/FileName.c
+++ Project2/FileName.c
@@ -1,28 +1,15 @@
 #include <stdio.h> 
 
-//10번: 오른쪽 정렬 직삼각형 모양으로 n줄짜리 별 찍기
+//11번: T를 입력받지 않고도 입력받은 게 T줄이면 T줄만큼 A+B를 출력, 마지막엔 항상 A==0 B==0
+
 int main() {
-	int n, i, j, k;
-	scanf("%d", &n);
-	//n개의 줄 출력
-	for (i=1;i<=n;i++) {
-		//공백 출력 (공백은 n-1개씩)
-		for (j=i;j<n;j++) {
-			printf(" ");
-		}
-		//별 출력 (별은 i개수랑 똑같이)
-		for (k=1;k<=i;k++) {
-			printf("*");
-		}
-		//줄바꿈 & 마지막 출력에는 줄바꿈 안되게..
-		if (i != n) {
-			printf("\n");
-		}
-		else {
+	int A, B;
+	while (1) {
+		scanf("%d %d", &A, &B);
+		if (A == 0 && B == 0) {
 			break;
 		}
+		printf("%d\n", A + B);
 	}
 	return 0;
 }
-
-
