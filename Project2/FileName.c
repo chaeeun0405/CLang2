89515452b35d5c51c5f5d58dc2759b515fca718c
chaeeun <angelasce@naver.com>
2023-07-04 01:52:47 +0900

    3x3 배열을 만들어서 아무런 값을 할당

--- Project2/FileName.c
+++ Project2/FileName.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
 int main() {
-	//포 인 터
-		int *pa; //int형 포인터 변수 pa 선언
-		int n = 3;
-		pa = &n; //pa에 n의 주소 대입
-		printf("%d\n", n); //n의 값
-		printf("%d\n", *pa); //n의 값 (주소(pa)가 가리키는(*) 값을 출력해~)
-		printf("%d\n", pa); //n의 주소
-		printf("%d\n", &n); //n의 주소
-
+	//3x3 배열을 만들어서 아무런 값을 할당
+	int i, k;
+	int n[3][3];
+	for (i=0;i<3;i++) {
+		for (k=0;k<3;k++) {
+			n[i][k] = i * k;
+		}
+	}
+	for (i = 0; i < 3; i++) {
+		for (k = 0; k < 3; k++) {
+			printf("%d", n[i][k]);
+		}
+		printf("\n");
+	}
+		     
 }
