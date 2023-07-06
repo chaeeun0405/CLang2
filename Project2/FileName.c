dbe180657ad910bddad832dd322a5a650644ba4b
chaeeun <angelasce@naver.com>
2023-07-07 04:49:24 +0900

    번호 안 불린 사람 찾기

--- Project2/FileName.c
+++ Project2/FileName.c
@@ -1,29 +1,20 @@
 #include <stdio.h>
 
-// N개의 바구니. 
-// 바구니 번호와 같은 숫자의 공
-// M번 공 바꿈
-// 바구니 2개 골라서 서로 바꿈
-
+//번호 안 불린 사람 찾기
 int main() {
-	int N, M, i, j;
-	int ball[100];
-	scanf("%d %d", &N, &M);
-	
-	for (int a = 0; a < N; a++) {          //바구니에 자기 숫자 공 넣어주기
-		ball[a] = a + 1;      //공은 1번부터
+	int n;
+	int stu[30] = { 0 };
+
+	//과제 낸 번호
+	for (int i = 0; i < 28; i++) {
+		scanf("%d", &n);
+		stu[n - 1] = n;
 	}
 
-	for (int a = 0; a < M ; a++) {          //공 바꾸기
-		scanf("%d %d", &i, &j);
-		int A = ball[i-1];
-		int B = ball[j-1];
-		ball[i-1] = B;
-		ball[j-1] = A;
+	for (int i = 0; i < 30; i++) {
+		if (stu[i] == 0) {
+			printf("%d\n", i + 1);
+		}
 	}
-	for (int a = 0; a < N; a++) {
-		printf("%d ", ball[a]);
-	}
-
 	return 0;
 }
