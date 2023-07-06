98ac4c684016d20a86281f2aa3003c1d375357a1
chaeeun <angelasce@naver.com>
2023-07-07 04:46:39 +0900

    수열 A에서 X보다 작은 수를 모두 출력

--- Project2/FileName.c
+++ Project2/FileName.c
@@ -2,7 +2,7 @@
 
 //수열 A에서 X보다 작은 수를 모두 출력
 int main() {
-	int N, X; 수열 A에서 X보다 작은 수를 모두 출력
+	int N, X; //수열 A에서 X보다 작은 수를 모두 출력
 	int A[10000];
 	scanf("%d %d", &N, &X);
 	for (int i = 0; i < N; i++) {
