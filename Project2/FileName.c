af33d5e0bbf70741a8e233109d24e6001d18c970
chaeeun <angelasce@naver.com>
2023-07-13 17:59:01 +0900

    함수 기초구조

--- Project2/FileName.c
+++ Project2/FileName.c
@@ -1,31 +1,24 @@
 #include <stdio.h>
 
+int rain(int a, int b);
+void cal(int* a, int* b);
 
 int main() {
-    short M[2][2] = { {10, 20}, {30, 40} };
-    short* ap, * bp;
-    short a, sum = 0;
-
-    ap = M[0];
-    bp = M[0] + sizeof(M) / sizeof(M[0][0]) - 1; //sizeof(M) = 8 , sizeof(M[0][0]) = 2 / bp = M[0]+3
-    for (a = 0; a < sizeof(M) / sizeof(M[0][0]); a++) //(a=0 ; a<4 ; a++)
-        sum += *ap++ + *bp--; //sum = 250, ap의 참조값과 bp의 참조값을 더한 뒤 ap는 ++, bp는 --. 이때, 반복문의 마지막에도 ++ --가 들어간다는 점에 주의
-
-    *--ap = 45; //M[2][3] 되어있는 걸 M[2][2]로 되돌리고 값 입력 (10 20 30 45)
-    *++bp = 15; //M[0][-1] 되어있는 걸 M[0][0]로 되돌리고 값 입력 (15 20 30 45)
-    a = 0;
-
-    bp[++a]++; //bp[1] = M[0][1] + 1 = 21
-    printf("%d %d %d %d\n", M[0][0], M[0][1], M[1][0], M[1][1]);
-    ++ap[-a--];
-    ap = M[0];
-    printf("%d %d %d %d\n", M[0][0], M[0][1], M[1][0], M[1][1]);
-
-    for (; a < sizeof(M) / sizeof(M[0][0]); a++) //값이 정해져있다면 굳이 초깃값을 지정할 필요는 없음
-        sum -= ap[a]; //sum = sum - ap[a] / sum == 88
-
-    ap = M[1] - sizeof(M[0]) / sizeof(M[0][0]); 
-    printf("%d\n", *ap);
-    printf("%d\n", sum);
-    printf("%d %d %d %d\n", M[0][0], M[0][1], M[1][0], M[1][1]);
+	int a = 10, b = 5;
+	printf("main 함수의 a b %d %d\n", &a, &b);
+	printf("%d\n", calc(a, b));
+	cal(&a, &b);
 }
+
+void cal(int* a, int* b) { //call by address, 원래 a값에 영향
+	*a = 2 * (*a) + (*b);
+	printf("address 함수의 a b:%d %d", a, b);
+}
+
+int calc(int a, int b) { //call by value, 값을 복사(원래 a에 영향 X)
+	a = 2 * a;
+	printf("value 함수의 a b: %d %d\n", &a, &b);
+	return a + b;
+}
+
+//call by value/address는 메인 함수와 부른 함수의 차원에 따라 다름! 차원이 다르면 add, 차원이 같으면 value
