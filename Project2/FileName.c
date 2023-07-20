24535516ff5ee3bbd63f0e5b30065ce7b60ba505
chaeeun <angelasce@naver.com>
2023-07-20 17:54:10 +0900

    함수, 문자열 출력(4)

--- Project2/FileName.c
+++ Project2/FileName.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include<conio.h>
 
-void good(int a) {
-    printf("%d개", a);
+int retvalue() {
+    int a, b;
+    a = 2, b = 3;
+    return a + b;
 }
+
 int main() {
-    int a;
-    scanf("%d", &a);
-    good(a);
+    printf("결과 = %d\n", retvalue()); //re 함수의 리턴값을 출력, 저렇게 해도 5가 출력됨
 }
+
+//이렇게도 가능
+//int main() {
+//    a = retvalue(); //5
+//    printf("결과 = %d\n", a);
+//}
