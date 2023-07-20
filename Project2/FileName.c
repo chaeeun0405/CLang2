4f5b4c299f222f8ea1bf3a2bfd94107dd9071a6d
chaeeun <angelasce@naver.com>
2023-07-20 17:46:42 +0900

    call by add vs call by val (1)

--- Project2/FileName.c
+++ Project2/FileName.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-int rain(int a, int b);
 void cal(int* a, int* b);
 
 int main() {
@@ -12,7 +11,7 @@ int main() {
 
 void cal(int* a, int* b) { //call by address, 원래 a값에 영향
 	*a = 2 * (*a) + (*b);
-	printf("address 함수의 a b:%d %d", a, b);
+	printf("address 함수의 a b:%d %d", &a, &b);
 }
 
 int calc(int a, int b) { //call by value, 값을 복사(원래 a에 영향 X)
@@ -22,3 +21,8 @@ int calc(int a, int b) { //call by value, 값을 복사(원래 a에 영향 X)
 }
 
 //call by value/address는 메인 함수와 부른 함수의 차원에 따라 다름! 차원이 다르면 add, 차원이 같으면 value
+//
+//
+//함수는 스택 구조로 동작
+//스택은 가장 나중에 들어온 게 가장 빨리 나가는 (끝나는) 구조
+//ex. 불리는 순서 A B C면 끝나는 순서 C B A 
